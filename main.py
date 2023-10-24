55e098a214833621adac08afed0e9fa238d8695d
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-24 14:51:51 +0400

    question bank

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-class Question:
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
 
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-
+QuizBrain()
 
