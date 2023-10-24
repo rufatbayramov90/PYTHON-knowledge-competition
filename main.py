c8020e0e7e92d52ff30fb679c001e6c4727cef01
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-24 16:15:04 +0400

    check answer

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-QuizBrain()
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
 
